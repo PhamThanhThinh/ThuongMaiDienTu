984b91cee949b0e70edab8a855627fa63fbf0acd
thinh <thinhqn999@gmail.com>
2024-09-09 22:39:49 +0700

    error javascript lib

---
+++ ThuongMaiDienTu/Areas/Admin/Attributes/AuthorizedAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ThuongMaiDienTu.Areas.Admin.Extensions;
+
+namespace ThuongMaiDienTu.Areas.Admin.Attributes
+{
+  public class AuthorizedAttribute : Attribute, IAuthorizationFilter
+  {
+    public string Code { get; set; } = "";
+
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+      //throw new NotImplementedException();
+      List<string> codes = context.HttpContext.Session.GetObject<List<string>>("codes");
+      if (codes.Count == 0 || !codes.Contains(Code))
+      {
+        context.Result = new UnauthorizedResult();
+      }
+    }
+  }
+}
---
+++ ThuongMaiDienTu/Areas/Admin/Extensions/SessionExtension.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+namespace ThuongMaiDienTu.Areas.Admin.Extensions
+{
+  public static class SessionExtension
+  {
+    public static T GetObject<T>(this ISession session, string key)
+    {
+      var value = session.GetString(key);
+      if (value == null)
+      {
+        return default(T);
+      }
+      return JsonConvert.DeserializeObject<T>(value);
+    }
+  }
+}
